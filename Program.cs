8ca1176d24abbf378ab602bd0fcb3a0b6ef5303f
Luis <m.luishernandez@outlook.com>
2020-10-27 03:23:01 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
